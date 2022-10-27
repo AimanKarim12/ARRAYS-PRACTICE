66b98778ba0d2e03dda4b0963777759225afec27
AimanKarim12 <113124249+AimanKarim12@users.noreply.github.com>
2022-10-27 09:34:16 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,39 @@
+#QUESTION 1
+list1 = ["January", "June", "July"]
+print(list1)
+
+#QUESTION 2
+list2 = []
+
+for x in range(700):
+
+  list2.append("joy")
+  print(list2)
+# #QUESTION 3
+# list3 = ["7"]
+
+# for x in list3:
+#   print(x * 700)
+ 
+# #QUESTION 4
+#   list4 = []
+
+#   for x in range(5000):
+#     import random
+
+#     print(random.randint(0, 100))
+
+# #QUESTION 5 
+#   list5 = []
+
+#   for x in range(300):
+#     import random
+
+#     print(random.randint(0, 40))
+
+# #QUESTION 7
+
+# str = "red,orange,yellow,green,blue,indigo,violet"
+
+# x = str.split(" ,  ")
+# print(x)
